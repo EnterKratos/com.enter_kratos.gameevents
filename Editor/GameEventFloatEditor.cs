9fd8db6b6dcdf81006182bf8d67baf5fe52afa17
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-10-01 17:02:28 +0100

    feat: Add GameEventFloat

---
+++ Editor/GameEventFloatEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EnterKratos.Unity.GameEvents.Editor
+{
+    [CustomEditor(typeof(GameEventFloat))]
+    public class GameEventFloatEditor : UnityEditor.Editor
+    {
+        private float _currentEventValue;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            _currentEventValue = EditorGUILayout.FloatField("Event Value", _currentEventValue);
+
+            if (GUILayout.Button("Raise Event"))
+            {
+                ((GameEventFloat)target)?.Raise(_currentEventValue);
+            }
+        }
+    }
+}
---
+++ Runtime/Listeners/GameEventListenerFloat.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace EnterKratos.Unity.GameEvents.Listeners
+{
+    public class GameEventListenerFloat : GameEventListener<float>
+    {
+        public GameEventFloat typedEvent;
+
+        [SerializeField]
+        private UnityEvent<float> typedResponse;
+
+        private void Awake()
+        {
+            Event = typedEvent;
+            Response = typedResponse;
+        }
+    }
+}
