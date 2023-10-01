c755f49e541cc00482b0dc067c7868a0f3b9a7c5
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-10-01 16:59:40 +0100

    feat: Add GameEventString

---
+++ Runtime/GameEventString.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace EnterKratos.Unity.GameEvents
+{
+    [CreateAssetMenu(menuName = "Game Events/Game Event String")]
+    public class GameEventString : GameEvent<string>
+    {
+    }
+}
