b19a7087bf66c0cd2dbf109ca90e8035b5ed3331
EnterKratos <15807441+EnterKratos@users.noreply.github.com>
2023-10-01 16:59:40 +0100

    feat: Add GameEventString

---
+++ Packages/com.enter_kratos.unity.gameevents/Editor/GameEventStringEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace EnterKratos.Unity.GameEvents.Editor
+{
+    [CustomEditor(typeof(GameEventString))]
+    public class GameEventStringEditor : UnityEditor.Editor
+    {
+        private string _currentEventValue;
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            _currentEventValue = EditorGUILayout.TextField("Event Value", _currentEventValue);
+
+            if (GUILayout.Button("Raise Event"))
+            {
+                ((GameEventString)target)?.Raise(_currentEventValue);
+            }
+        }
+    }
+}
---
+++ Packages/com.enter_kratos.unity.gameevents/Runtime/Listeners/GameEventListenerString.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace EnterKratos.Unity.GameEvents.Listeners
+{
+    public class GameEventListenerString : GameEventListener<string>
+    {
+        [SerializeField]
+        private GameEventString typedEvent;
+
+        [SerializeField]
+        private UnityEvent<string> typedResponse;
+
+        private void Awake()
+        {
+            Event = typedEvent;
+            Response = typedResponse;
+        }
+    }
+}
